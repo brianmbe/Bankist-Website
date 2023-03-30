70dc05a2c4ccf57e62431cd0e4737f346dfab4cd
Brian Nyingambe <briannmbe@gmail.com>
2023-03-30 13:39:47 +0300

    complete the website

--- script.js
+++ script.js
@@ -257,19 +257,3 @@ imageTargets.forEach(img => imgObserver.observe(img));
   });
 })();
 ////////////////////////////////////////////////////////////////
-
-/*
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built!', e);
-});
-
-window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e);
-});
-
-window.addEventListener('beforeunload', function (e) {
-  e.preventDefault();
-  console.log(e);
-  e.returnValue = '';
-});
-*/
